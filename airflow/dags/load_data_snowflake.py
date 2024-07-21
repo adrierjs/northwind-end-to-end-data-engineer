9b00dc3c18199ff5a828d7b3f67927d30117e154
adrier <adrierj@gmail.com>
2024-07-21 20:30:22 -0300

    add - categories and customer_customer_demo

--- airflow/dags/load_data_snowflake.py
+++ airflow/dags/load_data_snowflake.py
@@ -88,4 +88,62 @@ load_data = SnowflakeOperator(
     dag=dag,
 )
 
+load_data = SnowflakeOperator(
+    task_id='load_and_merge_categories',
+    sql="""
+
+    USE DATABASE northwind;
+    CREATE OR REPLACE TEMPORARY TABLE staging_categories LIKE raw.categories;
+
+    -- Criação de tabela temporária 
+    COPY INTO staging_categories
+    FROM 's3://desafio-indicium/northwind/categories.csv'
+    STORAGE_INTEGRATION = s3_int
+    FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1 FIELD_DELIMITER = ';');
+
+    MERGE INTO raw.categories AS target
+    USING staging_categories AS source
+    ON target.category_id = source.category_id
+    WHEN MATCHED THEN
+    UPDATE SET
+        target.category_name = source.category_name,
+        target.description = source.description,
+        target.picture = source.picture
+    WHEN NOT MATCHED THEN
+    INSERT (category_id, category_name, description, picture) VALUES (source.category_id, source.category_name,
+    source.description, source.picture);
+    """,
+    snowflake_conn_id='snowflake_connection',
+    dag=dag,
+)
+
+load_data = SnowflakeOperator(
+    task_id='load_and_merge_customer_customer_demo',
+    sql="""
+    USE DATABASE northwind;
+
+    CREATE OR REPLACE TEMPORARY TABLE staging_customer_demo LIKE raw.customer_customer_demo;
+
+    -- Carregar dados na tabela de staging
+    COPY INTO staging_customer_customer_demo
+    FROM 's3://desafio-indicium/northwind/customer_customer_demo.csv'
+    STORAGE_INTEGRATION = s3_int
+    FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1 FIELD_DELIMITER = ';');
+
+    -- Inserir dados únicos na tabela final
+    MERGE INTO raw.customer_customer_demo AS target
+    USING staging_customer_customer_demo AS source
+    ON target.category_id = source.category_id
+    WHEN MATCHED THEN
+    UPDATE SET
+        target.category_id = source.category_id,
+        target.customer_type_id = source.customer_type_id
+    WHEN NOT MATCHED THEN
+    INSERT (category_id, customer_type_id) VALUES (source.category_id, source.customer_type_id
+    );
+    """,
+    snowflake_conn_id='snowflake_connection',
+    dag=dag,
+)
+
 load_data 
