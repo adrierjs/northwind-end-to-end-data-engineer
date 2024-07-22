ffafa22130963ba13d1adc04b4cc36db9c0ed516
adrier <adrierj@gmail.com>
2024-07-21 22:07:31 -0300

    add - employees

--- airflow/dags/load_data_snowflake.py
+++ airflow/dags/load_data_snowflake.py
@@ -146,4 +146,49 @@ load_data = SnowflakeOperator(
     dag=dag,
 )
 
+load_data = SnowflakeOperator(
+    task_id='load_and_merge_employees',
+    sql="""
+      USE DATABASE northwind;
+
+    CREATE OR REPLACE TEMPORARY TABLE staging_employees LIKE raw.employees;
+
+    -- Carregar dados na tabela de staging
+    COPY INTO staging_employees
+    FROM 's3://desafio-indicium/northwind/employees.csv'
+    STORAGE_INTEGRATION = s3_int
+    FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1 FIELD_DELIMITER = ';');
+
+    MERGE INTO raw.employees AS target
+    USING staging_employees AS source
+    ON target.employee_id = source.employee_id
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.last_name = source.last_name,
+            target.first_name = source.first_name,
+            target.title = source.title,
+            target.title_of_courtesy = source.title_of_courtesy, -- Corrigido o nome da coluna
+            target.birth_date = source.birth_date,
+            target.hire_date = source.hire_date,
+            target.address = source.address,
+            target.city = source.city,
+            target.region = source.region,
+            target.postal_code = source.postal_code,
+            target.country = source.country,
+            target.home_phone = source.home_phone,
+            target.extension = source.extension,
+            target.photo = source.photo,
+            target.notes = source.notes,
+            target.reports_to = source.reports_to,
+            target.photo_path = source.photo_path
+    WHEN NOT MATCHED THEN
+        INSERT (employee_id, last_name, first_name, title, title_of_courtesy, birth_date, hire_date, address, city, region, postal_code, country, home_phone, extension, photo, notes, reports_to, photo_path)
+        VALUES (source.employee_id, source.last_name, source.first_name, source.title, source.title_of_courtesy, source.birth_date, source.hire_date, source.address, source.city, source.region, source.postal_code, source.country, source.home_phone, source.extension, source.photo, source.notes, source.reports_to, source.photo_path);
+    """,
+    snowflake_conn_id='snowflake_connection',
+    dag=dag,
+)
+
+
+
 load_data 
